43054a72cd712fe2c7781afb9914839d8f87dd55
Dylan Collins <dcollins5590@students.mc3.edu>
2020-07-25 02:06:11 -0400

    figured out another solution

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -93,7 +93,8 @@ class SortingRobot:
         return self._light == "ON"
 
         
-
+    #-1 means the held item is less
+    #1 means the held item it more
     def sort(self):
         """
         Sort the robot's list.
@@ -102,32 +103,32 @@ class SortingRobot:
         self.swap_item()
 
         while self.light_is_on():
-            self.set_light_off() #start by turning it to false 
+
+            self.set_light_off() 
             self.move_right()
+            if self.can_move_right() is False and self.compare_item() == None:
+                return
 
-            #compare with the next number, swap it if it is smaller
-            #if there is anothewr n8umber, compare it to the foirst number. only swap if it's lower
-            while self.can_move_right():
-                if self.compare_item() == -1: 
-                    self.swap_item()
-                self.move_right() 
-
-            #if there is n further number and its smaller, swap
-            while self.can_move_left():
-                if self.can_move_right() == False and self.compare_item() == -1:
-                    self.swap_item()
+            if self.compare_item() == None:
                 self.swap_item()
+                self.set_light_on()
 
-            #if the current number is higher, swap and turn ligh on, else move left
-                if self.compare_item() == 1: #If the held item's value is greater,
+            while self.can_move_right():
+                if self.compare_item() == 1: 
                     self.swap_item()
                     self.set_light_on()
+                self.move_right() 
+
+            if self.compare_item() == 1:
+                self.swap_item()
+
+            while self.can_move_left():
                 self.move_left()
-        #if its false move left and swap
-        while self.can_move_left():
-            self.move_left()  
-        self.swap_item()
-           
+
+            while self.compare_item() is not None:
+                self.move_right()
+
+            self.swap_item()
 
 
 if __name__ == "__main__":
@@ -135,7 +136,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-    l = [5,2,3,4,1]
+    l = [5,4,3,2,1]
     robot = SortingRobot(l)
 
     robot.sort()
